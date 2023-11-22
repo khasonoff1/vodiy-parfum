156886a32537432e672f29f654d4084d4e01429c
khasonoff1 <khasonoff1@gmail.com>
2023-11-22 21:51:18 +0500

    nhujhbhsafbhdhjnehfdhvdfjdf

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,7 +5,7 @@ import { TOKEN } from "@/constants";
 
 const request = axios.create({
     baseURL: `https://vodiy-parfum-backend.vercel.app/api/v1/`,
-    timeout: 20000,
+    timeout: 10000,
     headers: { Authorization: `Bearer ${Cookies.get(TOKEN)}` },
 
 })
